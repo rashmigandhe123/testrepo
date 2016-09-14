73cfcee7ad286844b3db0b3d560c6a4b7e1ca47a
rashmi <rashmigandhe06@gmail.com>
2016-09-14 12:36:41 +0100

    test class file

---
+++ Test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rashmi
+ * Date: 9/14/2016
+ * Time: 12:34 PM
+ */
+
+namespace MyProject;
+
+
+class Test
+{
+    public function myFirstFunction(){
+
+    }
+}
